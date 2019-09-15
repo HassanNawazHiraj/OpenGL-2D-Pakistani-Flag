e2c1dd98b1199f0f96f543003c65ec1616c2f3a3
Hassan Nawaz <hassancent@gmail.com>
2019-09-15 19:33:46 +0500

    flag done

--- OpenGL 2D Pakistan flag/OpenGL 2D Pakistan flag.cpp
+++ OpenGL 2D Pakistan flag/OpenGL 2D Pakistan flag.cpp
@@ -12,7 +12,7 @@ int main(int argc, char** argv)
 	glutInit(&argc, argv);
 	glutInitWindowSize(600, 600);
 	glutInitWindowPosition(100, 100);
-	glutCreateWindow("Android/Robot");
+	glutCreateWindow("Pakistani Flag");
 	//Step#2 Do Open GL related initializations
 	init();
 	//Step#3 Register Call back methods or Event Handler methods
@@ -24,8 +24,8 @@ int main(int argc, char** argv)
 
 void init(void) {
 	// select background color
-	glClearColor(1.0, 1.0, 1.0, 0.0); // (red, green, blue, alpha), used by glClear
-
+	glClearColor(0.82, 1.0, 0.823, 0.0); // (red, green, blue, alpha), used by glClear
+	
 	// Define world coordinate frame
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
@@ -45,6 +45,42 @@ void drawCircle(float cx, float cy, float r, int num_segments) {
 	glEnd();
 }
 
+void drawStick(float topLeftX, float topLeftY, float width, float height) {
+	glBegin(GL_QUADS);
+	glVertex3f(topLeftX, topLeftY, 0.0);
+	glColor3ub(126, 79, 52);
+	glVertex3f(topLeftX + width, topLeftY, 0.0);
+	glColor3ub(185, 122, 87);
+	glVertex3f(topLeftX + width, topLeftY + height, 0.0);
+	glColor3ub(126, 79, 52);
+	glVertex3f(topLeftX, topLeftY + height, 0.0);
+	glEnd();
+}
+
+void drawFlagGreenBox(float topLeftX, float topLeftY, float width, float height) {
+	glBegin(GL_QUADS);
+	glVertex3f(topLeftX, topLeftY, 0.0);
+	glColor3ub(40, 210, 91);
+	glVertex3f(topLeftX + width, topLeftY, 0.0);
+	glColor3ub(24, 126, 55);
+	glVertex3f(topLeftX + width, topLeftY + height, 0.0);
+	glColor3ub(152, 235, 176);
+	glVertex3f(topLeftX, topLeftY + height, 0.0);
+	glEnd();
+}
+
+void drawWhiteBox(float topLeftX, float topLeftY, float width, float height) {
+	glBegin(GL_QUADS);
+	glVertex3f(topLeftX, topLeftY, 0.0);
+	glColor3ub(229, 229, 229);
+	glVertex3f(topLeftX + width, topLeftY, 0.0);
+	glColor3ub(240, 240, 240);
+	glVertex3f(topLeftX + width, topLeftY + height, 0.0);
+	glColor3ub(193, 193, 193);
+	glVertex3f(topLeftX, topLeftY + height, 0.0);
+	glEnd();
+}
+
 void drawBox(float topLeftX, float topLeftY, float width, float height) {
 	glBegin(GL_QUADS);
 	glVertex3f(topLeftX, topLeftY, 0.0);
@@ -54,9 +90,51 @@ void drawBox(float topLeftX, float topLeftY, float width, float height) {
 	glEnd();
 }
 
+void drawStar() {
+	glColor3ub(255, 255, 255);
+	glLineWidth(4);
+
+	glBegin(GL_LINE_STRIP);
+	glVertex3f(-0.001, 0.28, 0);
+	glVertex3f(0.01, 0.314, 0.0);
+	glVertex3f(0.05, 0.314, 0.0);
+	glVertex3f(0.017, 0.336, 0.0);
+	glVertex3f(0.03, 0.371, 0.0);
+	glVertex3f(-0.002, 0.35, 0.0);
+	glVertex3f(-0.032, 0.371, 0.0);
+	glVertex3f(-0.022, 0.337, 0.0);
+	glVertex3f(-0.05, 0.314, 0.0);
+	glVertex3f(-0.013, 0.314, 0.0);
+	glVertex3f(-0.001, 0.28, 0);
+
+	glEnd();
+}
+
+
+
+void drawMoon() {
+	glColor3ub(255, 255, 255);
+	drawCircle(-0.05, 0.33, 0.11, 80);
+	glColor3ub(34, 177, 76);
+	drawCircle(-0.01, 0.33, 0.09, 80);
+}
+
 
 void display(void) {
 	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3ub(185, 122, 87);
+	drawStick(-0.4, -0.3, 0.05, 0.8);
+	glColor3ub(34, 177, 76);
+	drawBox(-0.35, 0.18, 0.6, 0.3);
+	glColor3f(1.0, 1.0, 1.0);
+	//draw the white on flag
+	drawWhiteBox(-0.35, 0.18, 0.1, 0.3);
+	drawMoon();
+	drawStar();
+	//glColor3ub(255, 255, 255);
+	//drawBox(-0.05, 0.28, 0.1, 0.1);
+		
+
 
 	
 	glFlush();
